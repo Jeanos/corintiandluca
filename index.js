7b42b75c5b5104f15d4964c9c63387596aae90b5
LucaMattia <lucamat1997@gmail.com>
2018-10-11 21:34:45 -0700

    added to index.js, by:Luca

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-<<<<<<< HEAD
-setInterval (()=>{
+//A
+function RandomRed(){
 
-=======
+}
+
 setInterval(()=> {
 app.style.backgroundColor = "rgb(255,222,255)";
->>>>>>> 42fd7ec3af67a4a0b0d23665e0b6f672ca662994
 }, 500);
+
